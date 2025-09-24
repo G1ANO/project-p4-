a8b66ac70fb34a8507ba10d0baa04934163facd9
donald kiarie <donaldkiarie768@gmail.com>
2025-09-24 10:36:44 +0300

    add more files

---
+++ backend/models.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from database import db, bcrypt   
+
+class User(db.Model):
+    _tablename_ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    subscriptions = db.relationship("Subscription", back_populates="user", cascade="all, delete-orphan")
+
+    def set_password(self, password):
+     
+        self.password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
